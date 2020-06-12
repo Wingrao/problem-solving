4bdb0c0e0ac093e4674286ec9d466bd8ff2dc341
Wingrao <56922415+Wingrao@users.noreply.github.com>
2020-06-12 07:59:26 +0200

    Day5

---
+++ daily-coding-problem/Day5.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <utility> 
+using namespace std; 
+  
+int main() 
+{ 
+    pair <int, int> cons ; 
+  
+    cons.first = 3; 
+    cons.second = 4; 
+  
+  public void car (const pair<int, int> x){
+     cout << x.first << endl;
+  }
+
+  
+  public void cdr (const pair<int, int> x){
+     cout << x.second << endl;
+  }
+   
+    return 0; 
+} 
